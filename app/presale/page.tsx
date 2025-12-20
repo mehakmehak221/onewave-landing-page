636f9a74f3a61cb7ea171f65327bb02bbca8d252
mehakmehak221 <mehak.gaur@maxtron.ai>
2025-12-20 13:30:59 +0530

    added components in presale page

--- app/presale/page.tsx
+++ app/presale/page.tsx
@@ -1,9 +1,18 @@
 'use client';
 
+import Header from '../components/Header';
+import ComingSoon from '../components/ComingSoon';
+import RWA from '../components/RWA';
+import Newsletter from '../components/Newsletter';
+
+
 export default function Presale() {
     return (
         <div>
-            <h1>Presale</h1>
+           <Header />
+           <ComingSoon />
+           <RWA/>
+           <Newsletter/>
         </div>
     );
 }
