c5ae8a7d2b46eadc23f236f51b1d92c946155194
developwithanil <anil@maxtron.ai>
2025-12-20 18:49:40 +0530

    build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import SiteLoader from './components/SiteLoader';
 
 export const metadata: Metadata = {
   title: "Onewave - Multi-Chain Multi-Utility Platform",
   description: "Cutting-ceiling multi-can, multi-utility cryptocurrency platform designed to provide users with a comprehensive suite of blockchain-based tools and services.",
-  viewport: "width=device-width, initial-scale=1, maximum-scale=5",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 5,
 };
 
 export default function RootLayout({
